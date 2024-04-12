0123907722d7b741b7486d9d5601fced0cda6482
Mpho <st10217639@vcconnect.edu.za>
2024-04-12 13:36:01 +0200

    Mpho - Timesheet entry and category list code

--- app/src/main/java/com/example/timeglimpse/CartegoriesActivity.kt
+++ app/src/main/java/com/example/timeglimpse/CartegoriesActivity.kt
@@ -1,13 +1,11 @@
 package com.example.timeglimpse
 
-import android.annotation.SuppressLint
+
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class CartegoriesActivity : AppCompatActivity() {
     private val categoryList = mutableListOf<String>()
