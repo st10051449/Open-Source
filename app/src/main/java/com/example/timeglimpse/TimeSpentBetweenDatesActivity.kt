45d65224c9312bddffbefa0041045d4522fdae20
ST10033321 <104824791+ST10033321@users.noreply.github.com>
2024-05-03 14:26:10 +0200

    part 2 OPSC7311 st10033321

--- app/src/main/java/com/example/timeglimpse/TimeSpentBetweenDatesActivity.kt
+++ app/src/main/java/com/example/timeglimpse/TimeSpentBetweenDatesActivity.kt
@@ -1,6 +1,8 @@
 package com.example.timeglimpse
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +11,12 @@ import androidx.core.view.WindowInsetsCompat
 class TimeSpentBetweenDatesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_time_spent_between_dates)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnNavigationOptions: Button = findViewById(R.id.btnNavigationOptions3)
+        // Set onClickListener for Navigation Options button
+        btnNavigationOptions.setOnClickListener {
+            startActivity(Intent(this, MenuActivity::class.java))
         }
     }
 }
